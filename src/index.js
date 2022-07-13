01227e538bd06956ef620f9dbc76418cf6e10945
moshimoshi234 <96729163+moshimoshi234@users.noreply.github.com>
2022-07-13 18:12:43 +0900

    jsの基礎

--- src/index.js
+++ src/index.js
@@ -1,10 +1,67 @@
-import "./styles.css";
+/**
+ * 分割代入
+ */
+// const myprofile = {
+//   name: "Ken",
+//   age: 28
+// };
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const { name, age } = myprofile;
+
+// const message2 = `名前は${name}です。年齢は${age}歳です。                                                                                           `;
+
+// console.log(message2);
+
+/**
+ * スプレッド構文
+ */
+// 配列の展開
+// const array = [1, 2];
+// const sum = (num1, num2) => console.log(num1 + num2);
+// sum(array[0], array[1]);
+// sum(...array);
+// 上と下は同じ処理をする！！
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// // コピー
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// // 結合
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// mapやfilterを使った配列の処理
+// 配列の要素をそれぞれ取り出す
+const nameArr = ["田中", "山田", "佐藤"];
+
+// 従来の方法
+// for (let i = 0; i < nameArr.length; i++) {
+//   console.log(nameArr[i]);
+// }
+
+// mapを使った方法
+// nameArr.map((name) => console.log(name));
+
+// filter
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// 三項演算子
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています！！" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 60));
